3e6e34a9999885616372e5d72928cabf8ece1f51
Kikuro <45486613+Kikuro@users.noreply.github.com>
2018-12-22 17:29:49 +0100

    add score

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 #include "Flappy.h"
+#include <iostream>
 
 using namespace sf;
+using namespace std;
 
 int main()
 {
@@ -19,21 +21,31 @@ int main()
     Sprite ptak(t3);
     ptak.setOrigin(30,30);
 
+    Font f1;
+    f1.loadFromFile("arial.ttf");
+    Text text;
+    text.setFont(f1);
+    text.setFillColor(Color::Black);
+
     int Field[7];
     int Puzzle[7];
-    float timer=0, delay=0.01;
+    float timer=0, delay=0.009;
+    unsigned int score=0, highscore=getHighscore();
 
     for(int i=0; i<7; i++)
     {
         Field[i]=i*150;
-        Puzzle[i]= rand()%600;
+        Puzzle[i]= rand()%570;
     }
 
     Clock clock;
 
     int x=100, y=300;
     float dx=0, dy=0;
-    bool isJump=false;
+    char goNext = NULL;
+    bool isJump=false, gameOver=false, isPause=false, next=false, next1=false;
+
+    y=597-Puzzle[1];
 
     while(window.isOpen())
     {
@@ -54,11 +66,12 @@ int main()
         {
             for(int i=0;i<7;i++)
             {
-                Field[i]+=-1;
+                if(isPause==false)
+                    Field[i]+=-1;
             }
-            if(Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
-            if(Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
-            if(Keyboard::isKeyPressed(Keyboard::Space)) isJump=true;
+            if(Keyboard::isKeyPressed(Keyboard::Right) && isPause==false) x+=3;
+            if(Keyboard::isKeyPressed(Keyboard::Left) && isPause==false) x-=3;
+            if(Keyboard::isKeyPressed(Keyboard::Space) && isPause==false) isJump=true;
 
             dy+=0.2;
             y+=dy;
@@ -69,6 +82,7 @@ int main()
                 dy=-4;
                 isJump=false;
             }
+            next1=false;
             timer=0;
         }
 
@@ -78,6 +92,52 @@ int main()
             {
                 Field[i]=800;
                 Puzzle[i]=rand()%600;
+            }
+        }
+
+        for(int i=0;i<6;i++)
+        {
+            if(i%2==1)
+            {
+                if(x>=(Field[i]+26) && x<=(Field[i]+118))
+                {
+                    if(y>504-Puzzle[i] && y<690-Puzzle[i])
+                    {
+                        goNext = i;
+                    }
+                    else
+                        gameOver=true;
+                }
+            }
+        }
+
+        if(!(goNext==NULL))
+        {
+            int i=goNext;
+            if(x>=(Field[i]+118)&&x<=(Field[i]+119))
+            {
+                if(next1==false)
+                {
+                    next=true;
+                }
+            }
+            goNext=NULL;
+        }
+
+        if(next)
+        {
+            score+=1;
+            next=false;
+            next1=true;
+        }
+
+        if(gameOver)
+        {
+            isPause=true;
+            if(highscore<score)
+            {
+                setNewHighscore(score);
+                //add game over screen
             }
         }
 
@@ -97,6 +157,14 @@ int main()
                    window.draw(mapa);
            }
         }
+
+        unsigned int hScore = static_cast<unsigned int>(score);
+        string _str = to_string(hScore);
+        text.setString(_str);
+        text.setCharacterSize(20);
+        text.setPosition(0.f,0.f);
+        window.draw(text);
+
         ptak.setPosition(x,y);
         window.draw(ptak);
         window.display();
