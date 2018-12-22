cf0546eb25a8f919f3600bde849c6e6e57bfb1f6
Kikuro <45486613+Kikuro@users.noreply.github.com>
2018-12-22 12:51:39 +0100

    add flying bird

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 #include "Flappy.h"
 
 using namespace sf;
 
 int main()
 {
+    srand(time(NULL));
+
     RenderWindow window(VideoMode(800,800),"Flappy Bird",Style::Close);
-    Texture t1;
-    t1.loadFromFile("testMapa.png");
+    Texture t1, t2, t3;
+    t1.loadFromFile("Mapa.png");
+    t2.loadFromFile("Tlo.png");
+    t3.loadFromFile("Ptak.png");
 
     Sprite mapa(t1);
-    mapa.setTextureRect(IntRect(0,400,150,800));
+    Sprite tlo(t2);
+    Sprite ptak(t3);
+    ptak.setOrigin(30,30);
 
     int Field[7];
-    float timer=0, delay=0.009;
+    int Puzzle[7];
+    float timer=0, delay=0.01;
 
     for(int i=0; i<7; i++)
     {
         Field[i]=i*150;
+        Puzzle[i]= rand()%600;
     }
 
     Clock clock;
+
+    int x=100, y=300;
+    float dx=0, dy=0;
+    bool isJump=false;
 
     while(window.isOpen())
     {
@@ -43,6 +56,19 @@ int main()
             {
                 Field[i]+=-1;
             }
+            if(Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
+            if(Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
+            if(Keyboard::isKeyPressed(Keyboard::Space)) isJump=true;
+
+            dy+=0.2;
+            y+=dy;
+            if(y>770) y=769;
+            if(y<30) y=31;
+            if(isJump)
+            {
+                dy=-4;
+                isJump=false;
+            }
             timer=0;
         }
 
@@ -51,15 +77,28 @@ int main()
             if(Field[i]==-250)
             {
                 Field[i]=800;
+                Puzzle[i]=rand()%600;
             }
         }
 
         window.clear(Color::Black);
         for(int i=0;i<7;i++)
         {
-            mapa.setPosition(Field[i],0);
-            window.draw(mapa);
+           if(i%2==0)
+           {
+               tlo.setPosition(Field[i],0);
+               tlo.setTextureRect(IntRect(0,Puzzle[i],150,800));
+               window.draw(tlo);
+           }
+           if(i%2==1)
+           {
+                   mapa.setPosition(Field[i],0);
+                   mapa.setTextureRect(IntRect(0,Puzzle[i],150,800));
+                   window.draw(mapa);
+           }
         }
+        ptak.setPosition(x,y);
+        window.draw(ptak);
         window.display();
     }
 }
